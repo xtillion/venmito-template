38626033dfa505c8d79ff74cf3d2d1a3b9fbe12b
Antonio Tapia <antonio.tapia@upr.edu>
2025-02-27 12:51:22 -0400

    First Successful load of data into db

--- pythonScripts/initialRead.py
+++ pythonScripts/initialRead.py
@@ -23,7 +23,7 @@ with open('people.json', 'r') as file:
         # Define the SQL query to insert data
         insert_query = """
 INSERT INTO user_data (id, origin_id, first_name, last_name, telephone, email, enabled, user_devices,  location_city, location_country, create_date, delete_date, is_deleted)
-VALUES (%s, %s, %s,%s, %s, %s,%d, %s, %s,%s, %s, NULL, %d)"""
+VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, null, %s)"""
 
         # Define the data to be inserted
         arr = (str(uuid.uuid4()),user["id"],user["first_name"],user["last_name"],user["telephone"],user["email"],1,','.join(user["devices"]),user["location"]["City"],user["location"]["Country"],str(datetime.now()),0)
