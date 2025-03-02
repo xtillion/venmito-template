58fe93e554169d095c15de5eacd369e3183314f9
MarceloBarillas10 <marcelobarillas10@gmail.com>
2025-03-02 10:01:20 -0600

    feat: promotion bought?

--- data_viz.py
+++ data_viz.py
@@ -16,7 +16,7 @@ device_counts_df = people_data[['Android', 'iPhone', 'Desktop']].sum().reset_ind
 device_counts_df.columns = ['Device', 'Count']
 plt.figure(figsize=(6,6))
 plt.pie(device_counts_df['Count'], labels=device_counts_df['Device'], autopct='%1.1f%%', startangle=90, counterclock=False)
-plt.title('Device Count Distribution')
+plt.title('User Device Distribution')
 plt.savefig('figures/Device_Usage_Counts.png')  # Save figure
 
 ### CREATING VISUALIZATION FOR USER DISTRIBUTION BY COUNTRY
@@ -25,13 +25,13 @@ country.columns = ['Country', 'Count']
 country = country.sort_values(by='Count', ascending=False) 
 sns.set_theme(style='whitegrid')  
 plt.figure(figsize=(14, 7))
-ax = sns.barplot(x='Country', y='Count', data=country, palette='viridis')
+ax = sns.barplot(x='Country', y='Count', data=country, hue= 'Country', palette='viridis',legend=False)
 for p in ax.patches:
     ax.annotate(f'{int(p.get_height())}', (p.get_x() + p.get_width() / 2., p.get_height()),
                 ha='center', va='bottom', fontsize=10, color='black')
 plt.title('User Distribution by Country', fontsize=14, weight='bold')
 plt.xlabel('Country')
-plt.ylabel('Count')
+plt.ylabel('Total Users')
 plt.xticks(rotation=45)  
 # Grid and layout
 plt.grid(axis='y', linestyle='--', alpha=0.6)
@@ -45,7 +45,7 @@ responses.columns = ['Responses', 'Count']  # Rename columns
 plt.figure(figsize=(12, 6))  # Create new figure
 sns.barplot(x='Responses', y='Count', hue='Responses', data=responses)
 plt.title('Responses to Promotions')
-plt.xlabel('Response ')
+plt.xlabel('Response')
 plt.ylabel('Total')
 plt.savefig('figures/ResponsePlot.png')  # Save figure
 
@@ -130,11 +130,31 @@ def label_point(x, y, val, ax):
     adjust_text(texts, ax=plt.gca(), expand_points=(1.2, 1.5))
 label_point(item_sales_promo['Total Sales'], item_sales_promo['Total Promotions'], item_sales_promo['Item'], plt.gca())
 
+###CREATING VISUALIZATION FOR TRANSFER INTERNATIONAL VS DOMESTIC
 send_id_location = transfer_data.merge(people_data[['id','country','city']], left_on='sender_id',right_on='id', how='inner')
 send_id_location.rename(columns={'country':'sender_country','city':'sender_city'},inplace=True)
 send_id_location = send_id_location.merge(people_data[['id','country','city']], left_on='recipient_id',right_on='id', how='inner')
 send_id_location.rename(columns={'country':'reciepient_country','city':'recipient_city'},inplace=True)
 send_id_location['international'] = send_id_location.apply(lambda x: viz_help.check_if_international(x),axis = 1)
-print(send_id_location['international'].value_counts())
+transfer_international = send_id_location['international'].value_counts()
+plt.figure(figsize=(6, 6))
+transfer_international.plot.pie(autopct='%1.1f%%', startangle=90, colors=['#ff9999','#66b3ff'], labels=['Domestic', 'International'])
+plt.title('Distribution of International vs. Domestic Transfers')
+plt.ylabel('') 
+plt.savefig('figures/DistribInternationalDomestic.png')
 # send_recieve = transfer_data.merge(people_data[['id','country','city']], left_on='recipient_id',right_on='id', how='inner')
 # send_recieve = send_recieve['city'].value_counts().reset_index()
+
+
+###Checking if person buys what they're getting promotions on
+person_promo = promo_data[['phone','promotion','responded']]
+person_promo = person_promo.merge(transaction_data,on='phone',how='inner')
+person_promo['promo_bought'] = person_promo.apply(lambda x: viz_help.check_if_bought(x),axis = 1)
+promo_bought= person_promo['promo_bought'].value_counts().reset_index()
+plt.figure(figsize=(12, 6))  # Create new figure
+sns.barplot(x='promo_bought', y='count', hue='promo_bought', data=promo_bought)
+plt.title('Promotions vs Actual Purchase')
+plt.xlabel('Bought What Was Promoted')
+plt.ylabel('Count')
+plt.savefig('figures/PromotionBought.png')  # Save figure
+
--- helpers/data_viz_funcs.py
+++ helpers/data_viz_funcs.py
@@ -5,3 +5,11 @@ def check_if_international(row):
     if row['sender_country'] != row['reciepient_country']:
         return True
     return False
+
+def check_if_bought(row):
+    item_names = []
+    for item in row['items']:
+        item_names.append(item['name'])
+    if row['promotion'] in item_names:
+        return 'Yes'
+    return 'No'
