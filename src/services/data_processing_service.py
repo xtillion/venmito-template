ca1648f77a24a144831efd0a1fda18ca30a7f6d7
ControlYourPotatoes <alexanderpuga78@gmail.com>
2025-05-03 15:09:01 -0400

    Implement validation method in DataProcessingService: add _validate method for data validation using appropriate validators

--- src/services/data_processing_service.py
+++ src/services/data_processing_service.py
@@ -14,6 +14,12 @@ class DataProcessingService(ABC):
     def process(self, data: pd.DataFrame, **kwargs) -> Union[pd.DataFrame, Dict[str, pd.DataFrame]]:
         """Process data according to service-specific logic."""
         pass
+        
+    def _validate(self, data: pd.DataFrame, data_type: str) -> List[str]:
+        """Validate data using the appropriate validator."""
+        from src.data.validator import get_validator
+        validator = get_validator(data_type, data)
+        return validator.validate()
 
 
 class PeopleProcessingService(DataProcessingService):
